cc9729c9cd49176296e6ce523c5c240080b9856f
mohitkapittx <mohit.yeole@kapittx.com>
2025-05-20 23:37:17 +0530

    Update boss.java

--- boss.java
+++ boss.java
@@ -3,7 +3,9 @@ package boss;
 public class boss {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("loooooooooooooooooooooooooooooss");
+
+
 
 	}
 
